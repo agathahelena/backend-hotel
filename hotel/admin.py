b0a1a8b928b3415384ff5c3c68408831f1900378
yuriramos223 <gabriela.heloisa.ramos.ifc@gmail.com>
2025-06-23 09:26:20 -0300

    FEAT: FInalização da FEAT-1 #1

--- hotel/admin.py
+++ hotel/admin.py
@@ -1,2 +1,10 @@
 from django.contrib import admin
+from .models import Hóspede
+from .models import Quarto
+from .models import Pagamento
+from .models import FunçãoFuncionário
 
+admin.site.register(Hóspede)
+admin.site.register(Quarto)
+admin.site.register(Pagamento)
+admin.site.register(FunçãoFuncionário)
--- hotel/models.py
+++ hotel/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class Hóspede(models.Model):
+    nome = models.CharField(max_length=50)
+    cpf = models.CharField(max_length=14)
+    telefone = models.CharField(max_length=19)
+    dataNascimento = models.DateField(max_length=10)
+
+    def __str__(self):
+        return self.nome
+
+class Quarto(models.Model):
+    numero = models.CharField(max_length=4)
+    quantidadePessoas = models.CharField(max_length=1)
+
+    def __str__(self):
+        return self.numero
+
+class Pagamento(models.Model):
+    valor = models.CharField(max_length=5)
+    formaPagamento = models.CharField(max_length=15)
+    data = models.DateField(max_length=10)
+    hora = models.TimeField(max_length=3)
+
+    def __str__(self):
+        return self.valor
+
+class FunçãoFuncionário(models.Model):
+    nome = models.CharField(max_length=15)
+    funcionario = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.nome
